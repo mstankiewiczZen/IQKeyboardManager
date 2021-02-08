771146f08a7f878b422f3d5bfaa4f3093dcf6e6a
Maciej Stankiewicz <maciej.stankiewicz@zen.com>
2021-02-08 13:12:33 +0100

    Added spm for objc target, updated swift-tools-version to 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,12 +6,18 @@ import PackageDescription
 let package = Package(
     name: "IQKeyboardManagerSwift",
     products: [
-       .library(name: "IQKeyboardManagerSwift", targets: ["IQKeyboardManagerSwift"])
-   ],
-   targets: [
-       .target(
-           name: "IQKeyboardManagerSwift",
-           path: "IQKeyboardManagerSwift"
-       )
-   ]
+        .library(name: "IQKeyboardManagerSwift", targets: ["IQKeyboardManagerSwift"]),
+        .library(name: "IQKeyboardManager", targets: ["IQKeyboardManager"])
+    ],
+    targets: [
+        .target(
+            name: "IQKeyboardManagerSwift",
+            path: "IQKeyboardManagerSwift"
+        ),
+        .target(
+            name: "IQKeyboardManager",
+            path: "IQKeyboardManager",
+            publicHeadersPath: "IQKeyboardManager"
+        )
+    ]
 )
